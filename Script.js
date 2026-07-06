200530fd883a8c7e137f2ab177d2a4a7c193908f
Shekh-pixel <alifsar676@gmail.com>
2026-07-06 12:24:33 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      alert("✅ Product added to cart!\n\nWhatsApp: 9667633179");
+    });
+  });
+});
